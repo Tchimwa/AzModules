3bd6ec4e6aaf817eed36d9b40eb2b881ac9a3242
Tchimwa Sougang <56332566+Tchimwa@users.noreply.github.com>
2024-07-16 00:21:23 -0400

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = ">= 3.107.0"
-      configuration_aliases = [azurerm.vhub_subscription, azurerm.private_dnszone_subscription]
+      configuration_aliases = [azurerm.vhub_subscription, azurerm.private_dnszone_subscription, azurerm.remote_hub_vnet_subscription]
     }
   }
 }
